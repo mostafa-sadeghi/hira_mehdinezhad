b29a354d933725b80e7729729cb13ad39ffaad1d
msa <msai.sadeghi@gmail.com>
2023-11-20 18:59:44 +0330

    s

---
+++ bagels.py
@@ -0,0 +1,12 @@
+# import string
+# import random
+# def generate_secret_number(num_of_digits):
+#     all_digits = list(string.digits)
+#     random.shuffle(all_digits)
+
+
+
+
+numbers = [1,2,3,4,5]
+
+
---
+++ exercise1.py
@@ -0,0 +1,42 @@
+# message = 'hello every body'
+# print(type(message))
+# message = "hello every body"
+# print(type(message))
+
+# message = 'he said "something"'
+# print(message)
+
+# message = "he said \"something\""
+# print(message)
+
+# message="""hello every body, welcome to python class.
+# we are going to learn every things about python"""
+# print(message)
+# message="hello every body, welcome to python class.\nwe are going to learn every things about python"
+# print(message)
+
+
+
+# message = '''he said "something"'''
+# print(message)
+
+# name = "hira"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+
+# name = input("enter a name: ")
+# print(len(name))
+# print(name[0])
+# print(name[len(name)-1])
+# print(name[-1])
+
+name = "hello every body"
+print(name[0:3])
+# TODO   از متن بالا برش های زیر را ایجاد کن
+# llo
+# eve
+# body
+# ever
+# ell
